aed0b2d60888e25c317247b5b62610729ca01361
cyberaom <cyber_aom@hotmail.com>
2023-02-08 23:07:44 +0700

    addParameter

--- Aom.js
+++ Aom.js
@@ -12,11 +12,22 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   const Fname = req.body.Fname;
   const Lname = req.body.Lname;
-  console.log("First Name: " + Fname);
-  console.log("Last Name: " + Lname);
+  const mobileNO = req.body.mobileNO;
+  const email = req.body.email;
+  const income = req.body.income;
+  console.log("Received");
+  //console.log("First Name: " + Fname);
+  //console.log("Last Name: " + Lname);
   //res.send("Data received");
-  res.send("Data received</br>" + "First Name: " + Fname + "</br>Last Name: " + Lname);
-  //res.render("Test Date Received");
+  //res.send("Data received</br>" + "First Name: " + Fname + "</br>Last Name: " + Lname);
+  res.send([mobileNO
+    'Data received',
+    `First Name: ${Fname}`,
+    `Last Name: ${Lname}`,
+    `Mobile Number: ${mobileNO}`,
+    `E-mail: ${email}`,
+    `Income: ${income}`,
+  ]);
 
 });
  
